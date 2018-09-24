a6089729b6584708b1ba0cabe4d20a63a5c97fe0
sergcr1993 <37964219+sergcr1993@users.noreply.github.com>
2018-09-23 17:15:12 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * ** Author: Sergio Campos Ramirez
+ * ** Date: 09/23/2018
+ * ** Description: Test
+ * *********************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    
+    return 0;
+}
